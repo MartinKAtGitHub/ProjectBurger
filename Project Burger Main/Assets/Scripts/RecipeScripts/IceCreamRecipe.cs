6a31cbf442fb47f72afd9b0ba3c67300869e8c19
MartinK <martinsingh91@gmail.com>
2019-06-08 17:35:06 +0200

    Before testing, didnt do mucth just added some recipes

--- Project Burger Main/Assets/Scripts/RecipeScripts/IceCreamRecipe.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(menuName = "Ice cream recipe", fileName = "Recipes/IceCreamRecipe")]
-public class IceCreamRecipe : Recipe
-{
- 
-}

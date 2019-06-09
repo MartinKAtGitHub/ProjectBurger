cbd68c6c78e86b83316344f2f60618c5166df76f
MartinK <martinsingh91@gmail.com>
2019-06-09 15:54:05 +0200

    Removed Testing conent from TESTBRANCH

--- Project Burger Main/Assets/Scripts/Test.cs
+++ Project Burger Main/Assets/Scripts/Test.cs
@@ -5,19 +5,5 @@ using UnityEngine.EventSystems;
 
 public class Test : MonoBehaviour
 {
-    public  Vector2 offset; // How much away you want it to be from the hole
-    Vector2 newRespawnPosition; // the New position away from the hole
-    Vector2 playerPosition; // The saved position you get when fall.
-    GameObject player; // character object
-    public void Resapwn()
-    {
-        newRespawnPosition = playerPosition + offset;
-        Instantiate(player, newRespawnPosition, Quaternion.identity); // I dont know how you spawn your player but use the new pos
-    }
-
-    public void OnPointerExit(PointerEventData eventData)
-    {
-        Debug.Log("EXIT" + name);
-
-    }
+   // I removed this 
 }

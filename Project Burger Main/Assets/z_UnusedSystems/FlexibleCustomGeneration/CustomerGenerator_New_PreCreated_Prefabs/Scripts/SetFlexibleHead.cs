765a714736d72b188fda1b6097807cb209abd5d9
DESKTOP-4R8FPJR\andre <andre-lisle@hotmail.com>
2019-06-09 15:28:45 +0200

    Changed CustomerGenerator Abit

---
+++ Project Burger Main/Assets/z_UnusedSystems/FlexibleCustomGeneration/CustomerGenerator_New_PreCreated_Prefabs/Scripts/SetFlexibleHead.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SetFlexibleHead : MonoBehaviour {
+
+    public Image Head;
+    public Image Mouth;
+    public Image Eyes;
+    public Image Nose;
+    public Image HairHat;
+
+    public void SetImages(GameObject theHead) {
+        FlexibleCustomHead head = theHead.GetComponent<FlexibleCustomHead>();
+
+        Head.sprite = head.Head[Random.Range(0, head.Head.Length)];
+        Mouth.sprite = head.Mouth[Random.Range(0, head.Mouth.Length)];
+        Eyes.sprite = head.Eyes[Random.Range(0, head.Eyes.Length)];
+        Nose.sprite = head.Nose[Random.Range(0, head.Nose.Length)];
+        HairHat.sprite = head.HairHat[Random.Range(0, head.HairHat.Length)];
+
+        Head.transform.localPosition = theHead.transform.GetChild(0).transform.localPosition;
+        Mouth.transform.localPosition = theHead.transform.GetChild(3).transform.localPosition;
+        Eyes.transform.localPosition = theHead.transform.GetChild(1).transform.localPosition;
+        Nose.transform.localPosition = theHead.transform.GetChild(2).transform.localPosition;
+        HairHat.transform.localPosition = theHead.transform.GetChild(4).transform.localPosition;
+
+    }
+}

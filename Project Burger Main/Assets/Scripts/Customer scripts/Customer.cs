f371e903cf39989367d21fe772c50e8bc6caaf49
MartinK <martinsingh91@gmail.com>
2019-06-17 20:32:53 +0200

    Started work on Customer Carousel

---
+++ Project Burger Main/Assets/Scripts/Customer scripts/Customer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(OrderGenerator))]
+public class Customer : MonoBehaviour
+{
+
+    public OrderGenerator OrderGenerator { get; private set; }
+
+    private void Awake()
+    {
+        OrderGenerator = GetComponent<OrderGenerator>();
+        
+    }
+}
